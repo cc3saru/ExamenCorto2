01b5730fc737ad93b654bda7344c052f2c4ac530
cc3sar <cesarjosue12.200@gmail.com>
2023-08-04 20:50:05 -0600

    Agregar cards dinamicos

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-// Seleccionar el elemento h1 y cambiar su texto
-// document.querySelector('h1').textContent = 'Hola mundo';
+const main = document.querySelector('.main');
+
+for (let i = 0; i < 10; i++) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = `https://picsum.photos/200/300?random=${i}`;
+  img.alt = 'Card image';
+  card.appendChild(img);
+
+  const description = document.createElement('div');
+  description.classList.add('card-description');
+
+  const p = document.createElement('p');
+  p.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel bibendum bibendum, elit sapien bibendum sapien, vel bibendum sapien sapien vel sapien.';
+  description.appendChild(p);
+
+  card.appendChild(description);
+  main.appendChild(card);
+}
